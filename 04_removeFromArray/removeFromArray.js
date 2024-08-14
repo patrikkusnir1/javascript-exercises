49bb9a5e352fc7957d091c65f19e18380d34fec5
patrikkusnir1 <patrikkusnir1@gmail.com>
2024-08-14 19:13:32 +0300

    compare to solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,11 @@
-const removeFromArray = function (array, ...deleteNums) {
-  // loop through each number in deleteNums
-  for (let i = 0; i < deleteNums.length; i++) {
-    // current number to delete
-    let deleteNum = deleteNums[i];
-    // loop through the array to find the num to delete
-    for (let j = 0; j < array.length; j++) {
-      if (array[j] === deleteNum) {
-        array.splice(j, 1); // remove element
-        j--; // slide to next element what to be removed
-      }
+const removeFromArray = function (array, ...args) {
+  let newArray = [];
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
     }
-  }
-  return array;
+  });
+  return newArray;
 };
 
 // Do not edit below this line
