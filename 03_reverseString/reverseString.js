cf2007ffff1a86fe8b25a4740a232bb9643bf830
patrikkusnir1 <patrikkusnir1@gmail.com>
2024-08-14 19:02:42 +0300

    finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitString = string.split(""); // return new array
+  let reverseArray = splitString.reverse(); // reverse array
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...deleteNums) {
+  // loop through each number in deleteNums
+  for (let i = 0; i < deleteNums.length; i++) {
+    // current number to delete
+    let deleteNum = deleteNums[i];
+    // loop through the array to find the num to delete
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === deleteNum) {
+        array.splice(j, 1); // remove element
+        j--; // slide to next element what to be removed
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
