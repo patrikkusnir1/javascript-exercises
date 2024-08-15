956ebd3baef2844834fec42471b9eba3bd70e391
patrikkusnir1 <patrikkusnir1@gmail.com>
2024-08-15 11:25:00 +0300

    correct with solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
 const leapYears = function (year) {
-  if (year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0)) {
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
+
+  if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
     return true;
   } else {
     return false;
