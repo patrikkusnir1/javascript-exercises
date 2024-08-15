336a7484c6833e54ee38656e36bcfc2dea092350
patrikkusnir1 <patrikkusnir1@gmail.com>
2024-08-15 11:08:06 +0300

    finish 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // check if inputs are numbers, integers and non-negative
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  const min = Math.min(num1, num2);
+  const max = Math.max(num1, num2);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
